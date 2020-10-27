0a7a1de168bdc74bbc583939b3e86efe3c47dac8
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-27 10:15:53 +0530

    Add: Create Linked List

---
+++ LinkedList/LinkedList/LinkedLists.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedLists
+    {
+        internal Node head;
+        public LinkedLists()
+        {
+            this.head = null;
+        }
+    }
+}

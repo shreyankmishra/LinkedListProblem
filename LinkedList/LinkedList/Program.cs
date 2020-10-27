805789be5674197dbc185d99cb45a82322567ae8
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-27 11:52:55 +0530

    Add: Appending in Linked List

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -7,9 +7,9 @@ namespace LinkedList
         static void Main(string[] args)
         {
             LinkedLists list = new LinkedLists();
-            list.Add(70);
-            list.Add(30);
-            list.Add(56);
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
             list.Display();
         }
     }

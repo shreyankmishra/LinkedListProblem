b49e3e3f94c4451d40b1881d0d306939aad5b319
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-27 12:59:54 +0530

    Add: Test Size after Deleting element in Linked List

--- LinkedList/LinkedList/LinkedLists.cs
+++ LinkedList/LinkedList/LinkedLists.cs
@@ -101,6 +101,34 @@ namespace LinkedList
                 return removedNode;
             }
         }
+        public void Delete(int item)
+        {
+            Node temp = head, previous = null;
+            if (temp != null && temp.data == item)
+            {
+                head = temp.next;
+                return;
+            }
+            while (temp != null && temp.data != item)
+            {
+                previous = temp;
+                temp = temp.next;
+            }
+            if (temp == null)
+                return;
+            previous.next = temp.next;
+        }
+        public int Size()
+        {
+            int size = 0;
+            Node temp = head;
+            while (temp != null)
+            {
+                size++;
+                temp = temp.next;
+            }
+            return size;
+        }
         public bool Search(int item)
         {
             if (head == null)
--- LinkedList/LinkedListsTest/UnitTest1.cs
+++ LinkedList/LinkedListsTest/UnitTest1.cs
@@ -25,5 +25,16 @@ namespace LinkedListsTest
             list.Insert(3, 40);
             Assert.IsTrue(list.Search(40));
         }
+        [TestMethod]
+        public void TestSizeAfterDelete()
+        {
+            LinkedLists list = new LinkedLists();
+            list.Append(56);
+            list.Append(30);
+            list.Append(40);
+            list.Append(70);
+            list.Delete(40);
+            Assert.AreEqual(3, list.Size());
+        }
     }
 }
